86b5080807d1b5e85bed2274558153c82f5f5cda
rthanLingoport <rthan@lingoport.com>
2024-12-11 15:30:10 -0700

    Update listtmp.sh

--- bin/listtmp.sh
+++ bin/listtmp.sh
@@ -3,4 +3,4 @@ ls -l /tmp/*/*
 
 echo "test"
 
-ls -l /usr/local/tomcat/Lingoport_Data/LRM/staging/importkits/ftp/travelbank/web-app/TRANSLATED_KIT_9_fr_ca
+ls -lr /usr/local/tomcat/Lingoport_Data/LRM/staging/importkits/ftp/travelbank/web-app/
